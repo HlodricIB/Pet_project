2826b48ce8118cb3787ff640a9f2a8a418ac7d4b
Nikita <JPM_racer@mail.ru>
2022-03-19 14:10:59 +0300

    All_together added

--- ini_and_parser/include/parser.h
+++ ini_and_parser/include/parser.h
@@ -9,17 +9,26 @@
 
 class c_s_exception : public std::exception
 {
+private:
+    std::string error_message;
 public:
-    const char* what() const noexcept { return "File pet_project_config.ini not founded"; }
+    c_s_exception(const char* error_message_): error_message(error_message_) { }
+    const char* what() const noexcept { return error_message.c_str(); }
 };
 
 
 class Config_searching
 {
 private:
+    int level_down{0};
+    int level_up{0};
+    bool config_founded{false};
+    const char* config_to_find;
     std::string path;
 public:
     Config_searching(const char*);
+    void searching_forward (const char*, const char*);
+    void searching_backward (char*);
     const char* return_path() { return path.c_str(); }
 };
 
@@ -31,9 +40,9 @@ private:
     //host, hostaddr, port, dbname, password, connect timeout, client encoding, sslmode
     std::vector<std::string> values;
 public:
-    Parser() { }
-    Parser(const char*, const char*);
-    const char* v_r(std::vector<std::string>::size_type index) { return values[index].c_str(); }
+    Parser(int n) { values.reserve(n); }
+    Parser(const char*, const char*, int);
+    const char* parsed_info_index(std::vector<std::string>::size_type index) const { return values[index].c_str(); }
 };
 
 #endif // PARSER_H
--- ini_and_parser/src/parser.cpp
+++ ini_and_parser/src/parser.cpp
@@ -2,17 +2,65 @@
 #include <dirent.h>
 #include <unistd.h>
 #include <sys/types.h>
+#include <cstring>
 #include "parser.h"
 
-Config_searching::Config_searching(const char* config_to_find)
+Config_searching::Config_searching(const char* config_to_find_): config_to_find(config_to_find_)
 {
     char currentDir[1024];
-    struct dirent* entry;
-
-    bool config_founded(false);
-    while (!config_founded)
+    getcwd(currentDir, sizeof(currentDir));
+    char previousDir[1024];
+    std::strcpy(previousDir, currentDir);
+    //char new_previousDir[1024];
+    //char startDir[1024];
+    {
+        std::cout << "\n" << std::endl;
+        std::cout << currentDir << std::endl;
+        std::cout << previousDir << std::endl;
+        std::cout << "\n" << std::endl;
+    }
+    searching_forward(currentDir, previousDir);
+    {
+        std::cout << "\n" << std::endl;
+        std::cout << currentDir << std::endl;
+        std::cout << previousDir << std::endl;
+        std::cout << "\n" << std::endl;
+    }
+    while (level_up <= 3)
     {
         getcwd(currentDir, sizeof(currentDir));
+        {
+            std::cout << "\n" << std::endl;
+            std::cout << currentDir << std::endl;
+            std::cout << previousDir << std::endl;
+            std::cout << "\n" << std::endl;
+        }
+        searching_forward(currentDir, previousDir);
+        {
+            std::cout << "\n" << std::endl;
+            std::cout << currentDir << std::endl;
+            std::cout << previousDir << std::endl;
+            std::cout << "\n" << std::endl;
+        }
+        std::strcpy(previousDir, currentDir);
+        {
+            getcwd(currentDir, sizeof(currentDir));
+            std::cout << "\n" << std::endl;
+            std::cout << currentDir << std::endl;
+            std::cout << previousDir << std::endl;
+            std::cout << "\n" << std::endl;
+        }
+        //if (level_down == 0)
+            ++level_up;
+    }
+    searching_backward(currentDir);
+}
+
+void Config_searching::searching_backward(char* currentDir)
+{
+    struct dirent* entry;
+    while (!config_founded)
+    {
         DIR* dir = opendir(currentDir);
         chdir("../");
         while ((entry = readdir(dir)) != NULL)
@@ -23,19 +71,71 @@ Config_searching::Config_searching(const char* config_to_find)
                 {
                     path = std::string(currentDir) + '/' + std::string(entry->d_name);
                     config_founded = true;
-                    break;
+                    closedir(dir);
+                    return;
                 }
             }
         }
         closedir(dir);
         if (strcmp(currentDir, "/") == 0 && !config_founded)
         {
-            throw c_s_exception();
+            throw c_s_exception("Pet_project_config.ini not founded");
         }
     }
 }
 
-Parser::Parser(const char* config_filename, const char* section)
+void Config_searching::searching_forward (const char* currentDir, const char* previousDir)
+{
+    //if (level_down > 3 || config_founded)
+    //    return;
+    struct dirent* entry;
+    DIR* dir = opendir(currentDir);
+    while ((entry = readdir(dir)) != NULL)
+    {
+        {
+            //std::cout << entry->d_name << std::endl;
+        }
+        if (entry->d_type == DT_DIR)
+        {
+            if (strcmp(entry->d_name, ".") != 0
+                    && strcmp(entry->d_name, "..") != 0 && level_down <= 3)
+            {
+                ++level_down;
+                chdir(entry->d_name);
+                char currentDir[1024];
+                getcwd(currentDir, sizeof(currentDir));
+                if (strcmp(currentDir, previousDir) != 0)
+                {
+                    {
+                        char tcurrentDir[1024];
+                        getcwd(tcurrentDir, sizeof(currentDir));
+                        std::cout << tcurrentDir << std::endl;
+                    }
+                    searching_forward(currentDir, previousDir);
+                }
+                --level_down;
+            }
+        }
+        else
+        {
+            if (strcmp(entry->d_name, config_to_find) == 0)
+            {
+                path = std::string(currentDir) + '/' + std::string(entry->d_name);
+                config_founded = true;
+                closedir(dir);
+                return;
+            }
+        }
+    }
+    chdir("../");
+    closedir(dir);
+    {
+        std::cout << "\n" << currentDir << " changed to upper dir" << std::endl;
+        std::cout << currentDir << " closed\n" << std::endl;
+    }
+}
+
+Parser::Parser(const char* config_filename, const char* section, int n)
 {
     boost::property_tree::ptree config;
     try {
@@ -44,6 +144,7 @@ Parser::Parser(const char* config_filename, const char* section)
         std::cout << error.what() << std::endl;
     }
     const boost::property_tree::ptree& DB = config.get_child(section);
+    values.reserve(n);
     values.push_back(DB.get<std::string>("host"));
     values.push_back(DB.get<std::string>("hostaddr"));
     values.push_back(DB.get<std::string>("port"));
